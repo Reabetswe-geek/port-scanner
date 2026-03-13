d1dc0ebc3462c10821c497ec8a51346bec9382f8
Reabetswe <putukacaiphus@gmail.com>
2026-03-13 14:26:02 +0200

    Added basic Python thread port scanner

--- port-scanner.py
+++ port-scanner.py
@@ -1,15 +1,19 @@
 import socket
+import  threading
 
 target = input("Enter target IP: ")
 
 print("Scanning target:", target)
 
-for port in range(1, 1025):
+def scan_port(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     result = s.connect_ex((target, port))
 
     if result == 0:
-        print("Port", port,"is open")
+        print("Port", port, "is open")
 
     s.close()
+
+for port in range(1, 1025):
+    thread = threading.Thread(target=scan_port, args=(port,))
+    thread.start()
