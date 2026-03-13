cb350dcd4f8290988341d4fc5e5ba0cff45c97ed
Reabetswe <putukacaiphus@gmail.com>
2026-03-13 22:11:27 +0200

    Added threaded port scanner with service detection

--- port-scanner.py
+++ port-scanner.py
@@ -1,16 +1,33 @@
 import socket
 import  threading
 
-target = input("Enter target IP: ")
+target = "127.0.0.1"
 
-print("Scanning target:", target)
+services = {
+    21: "FTP",
+    22: "SSH",
+    23: "TELNET",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    130: "NetBIOS",
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB",
+    3389: "RDP"
+}
 
 def scan_port(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.settimeout(1)
+
     result = s.connect_ex((target, port))
 
     if result == 0:
-        print("Port", port, "is open")
+        service = service.get(port, "Unknown Service")
+        print(f"Port {port} is OPEN -> {service}")
 
     s.close()
 
